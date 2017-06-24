145e2366662748fd1368094920450c0a78c29a86
Михаил <Mihail.sverkynov@gmail.com>
2017-06-24 22:24:34 +0700

    123

---
+++ Model/BottomYashik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BottomYashik
+    {
+        public string Name;
+        public string Material;
+        public int Size;
+        public int Price;
+        //
+    }
+}
---
+++ Model/Penal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Penal
+    {
+        public string Name;
+        public string Material;
+        public int Size;
+        public int Price;
+    }
+}
